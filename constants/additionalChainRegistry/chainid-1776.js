11e7c97cc68f2e2f303c34fc205ddab20ce937c6
frani <frani@users.noreply.github.com>
2025-12-09 13:59:13 -0300

    Add RPCs to chainid-1776.js

--- constants/additionalChainRegistry/chainid-1776.js
+++ constants/additionalChainRegistry/chainid-1776.js
@@ -3,9 +3,20 @@ export const data = {
   "chain": "Injective",
   "icon": "injective",
   "rpc": [
-    "https://sentry.evm-rpc.injective.network",
-    "wss://sentry.evm-ws.injective.network",
+    "https://sentry.evm-rpc.injective.network"
+    "https://injective-rpc.publicnode.com", 
     "https://injectiveevm-rpc.polkachu.com",
+    "https://rpc-injective.imperator.co",
+    "https://injective-evm-rpc.highstakes.ch",
+    "https://injective-evm-rpc.scvsecurity.io",
+    "https://injective.json-rpc.decentrio.ventures",
+    "wss://sentry.evm-ws.injective.network",
+    "wss://injective-evm-rpc.highstakes.ch/ws",
+    "wss://injective-evm-ws.scvsecurity.io",
+    "wss://sentry.tm.injective.network/websocket",
+    "wss://injective-rpc.publicnode.com/websocket",
+    "wss://injectiveevm-rpc.polkachu.com/websocket",
+    "wss://rpc-injective.imperator.co/websocket",
     "wss://injectiveevm-ws.polkachu.com"
   ],
   "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
